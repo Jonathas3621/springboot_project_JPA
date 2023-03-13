6f45668f96eae0894610b280037beb5082aab89a
Jonathas dos Santos <jonathasdossantos007@gmail.com>
2023-03-13 15:15:49 -0300

    SubTotal & Total Methods

--- src/main/java/com/spring/course/entities/Order.java
+++ src/main/java/com/spring/course/entities/Order.java
@@ -99,7 +99,15 @@ public class Order implements Serializable{
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-
+	
+	public Double getTotal() {
+		Double total = 0.0;
+		for(OrderItem x: items) {
+			total += x.getSubTotal();
+		}
+		return total;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
